5154882c2ad536534a3699e9577bc94e750e28af
Vlad_Mudryy <vladyslav.mudryy@gmail.com>
2022-08-07 04:37:28 +0300

    add localStorage functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,18 @@
 'strict mode';
 
 window.addEventListener('DOMContentLoaded', () => {
+
     // Creating new todo
 
     const input = document.querySelector('.add-item__input'),
           inputSection = document.querySelector('.add-item'),
-          listParent = document.querySelector('.items__list'),
+          itemListParent = document.querySelector('.items__list'),
           itemQty = document.querySelector('.items__footer span'),
           itemsFilter = document.querySelectorAll('.items__footer li'),
           itemsClear = document.querySelector('.items__clear');
           
     class TodoItem {
-        constructor(text, className, classParent, isComplete = false) {
+        constructor(text, isComplete = false, className = 'item', classParent = '.items__list') {
             this.text = text;
             this.isComplete = isComplete;
             this.className = className;
@@ -20,7 +21,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const todoCard = document.createElement('div');
-            todoCard.classList.add(this.className); 
+            todoCard.classList.add(this.className);
+            if (this.isComplete === true) {
+                todoCard.classList.add('item-checked');
+            }
             todoCard.innerHTML = `
                 <label class="checkbox__container">
                     <input class="checkbox__input" type="checkbox">
@@ -33,15 +37,29 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // input 
+    // Load todos from localStorage
 
-    const arrayOfTodos = [];
+    const arrayOfTodos = [],
+          arrayOfLocal = [];
 
-    function createTodos() {
+    if (JSON.parse(localStorage.getItem('Todo-list'))) {
+        JSON.parse(localStorage.getItem('Todo-list')).forEach(item => {
+            arrayOfLocal.push(item);
+        });
+    }
+
+    localStorage.clear();
+    arrayOfLocal.forEach(item => {
+        itemListParent.innerHTML = '';
+        createTodos(item.text, item.isComplete);
+    });
+
+    // Add todos
+
+    function createTodos(text, status = false) {
         arrayOfTodos.push(new TodoItem(
-            input.value,
-            'item',
-            '.items__list'
+            text,
+            status
             ));
         input.value = '';
 
@@ -49,20 +67,34 @@ window.addEventListener('DOMContentLoaded', () => {
             item.render();
         });
         itemQty.textContent = arrayOfTodos.length;
+        localStorage.setItem('Todo-list', JSON.stringify(arrayOfTodos)); 
+        
+        const allItems = document.querySelectorAll('.item');
+        allItems.forEach(item => {
+        if (item.classList.contains('item-checked')) {
+            item.firstElementChild.firstElementChild.checked = true;
+            item.firstElementChild.lastElementChild.classList.add('text-line');
+        }
+        itemsFilter.forEach(item => {
+            item.classList.remove('on');
+        });
+        itemsFilter[0].classList.add('on');
+    });
     }
 
     input.addEventListener('keypress', event => {
         if (event.key === 'Enter' && input.value != '' && !input.value.match(/'^ *$'/)) {
-            listParent.innerHTML = '';
-                createTodos();
+            itemListParent.innerHTML = '';
+                createTodos(input.value);
         }
     });
 
     inputSection.addEventListener('click', event => {
         event.preventDefault();
-        if (event.target && event.target.classList.contains('add-submit') && input.value != '' && !input.value.match(/'^ *$'/)) {
-            listParent.innerHTML = '';
-            createTodos();
+        const target = event.target;
+        if (target && target.classList.contains('add-submit') && input.value != '' && !input.value.match(/'^ *$'/)) {
+            itemListParent.innerHTML = '';
+            createTodos(input.value);
         }
     });
 
@@ -70,12 +102,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function removeTodo(index) {
         arrayOfTodos.splice(index, 1);
+        localStorage.setItem('Todo-list', JSON.stringify(arrayOfTodos));
     }
 
-    listParent.addEventListener('click', event => {
-        if (event.target && event.target.classList.contains('checkbox__img')) {
-            removeTodo([...document.querySelectorAll('.item')].indexOf(event.target));
-            event.target.parentElement.remove();
+    itemListParent.addEventListener('click', event => {
+        const target = event.target;
+        if (target && target.classList.contains('checkbox__img')) {
+            removeTodo([...document.querySelectorAll('.item')].indexOf(target));
+            target.parentElement.remove();
             itemQty.textContent = arrayOfTodos.length;
         }
     });
@@ -86,27 +120,30 @@ window.addEventListener('DOMContentLoaded', () => {
         arrayOfTodos[index].isComplete = true;
         const todoBlock = document.querySelectorAll('.item');
         todoBlock[index].firstElementChild.lastElementChild.classList.add('text-line');
-        todoBlock[index].classList.add('item-checked');        
+        todoBlock[index].classList.add('item-checked');
     }
 
     function isCheckedFalse(index) {
         arrayOfTodos[index].isComplete = false;
         const todoBlock = document.querySelectorAll('.item');
         todoBlock[index].firstElementChild.lastElementChild.classList.remove('text-line');
-        todoBlock[index].classList.remove('item-checked'); 
+        todoBlock[index].classList.remove('item-checked');
     }
 
-    listParent.addEventListener('change', event => {
-        if (event.target && event.target.classList.contains('checkbox__input')) {
-            if (event.target.checked === true) {
-                isCheckedTrue([...document.querySelectorAll('.item')].indexOf(event.target.parentElement.parentElement));
-            } else if (event.target.checked === false) {
-                isCheckedFalse([...document.querySelectorAll('.item')].indexOf(event.target.parentElement.parentElement));
+    itemListParent.addEventListener('change', event => {
+        const target = event.target;
+        if (target && target.classList.contains('checkbox__input')) {
+            if (target.checked === true) {
+                isCheckedTrue([...document.querySelectorAll('.item')].indexOf(target.parentElement.parentElement));
+                localStorage.setItem('Todo-list', JSON.stringify(arrayOfTodos));
+            } else if (target.checked === false) {
+                isCheckedFalse([...document.querySelectorAll('.item')].indexOf(target.parentElement.parentElement));
+                localStorage.setItem('Todo-list', JSON.stringify(arrayOfTodos));
             }
         }
     });
 
-    // items filter (all/active/cpmpleted)
+    // Items filter (all/active/cpmpleted)
 
     itemsFilter[0].classList.add('on');
 
@@ -116,25 +153,28 @@ window.addEventListener('DOMContentLoaded', () => {
                 item.classList.remove('on');
             });
             item.classList.add('on');
-            if (event.target.id === 'all') {
-                document.querySelectorAll('.item').forEach(item => {
-                    console.log(item);
-                    item.classList.remove('hide');
-                });
-            } else if (event.target.id === 'active') {
-                document.querySelectorAll('.item').forEach(item => {
-                    item.classList.remove('hide');
-                    if (item.classList.contains('item-checked') !== false) {
-                        item.classList.add('hide');
-                    }
-                });
-            } else if (event.target.id === 'completed') {
-                document.querySelectorAll('.item').forEach(item => {
-                    item.classList.remove('hide');
-                    if (item.classList.contains('item-checked') !== true) {
-                        item.classList.add('hide');
-                    }
-                });
+            switch (event.target.id) {
+                case 'all': 
+                    document.querySelectorAll('.item').forEach(item => {
+                        item.classList.remove('hide');
+                    });
+                    break;
+                case 'active':
+                    document.querySelectorAll('.item').forEach(item => {
+                        item.classList.remove('hide');
+                        if (item.classList.contains('item-checked') !== false) {
+                            item.classList.add('hide');
+                        }
+                    });
+                    break;
+                case 'completed':
+                    document.querySelectorAll('.item').forEach(item => {
+                        item.classList.remove('hide');
+                        if (item.classList.contains('item-checked') !== true) {
+                            item.classList.add('hide');
+                        }
+                    });
+                    break;
             }
         });
     });
@@ -142,27 +182,8 @@ window.addEventListener('DOMContentLoaded', () => {
     // Clear all
 
     itemsClear.addEventListener('click', event => {
-        listParent.innerHTML = '';
+        itemListParent.innerHTML = '';
         arrayOfTodos.splice(0, arrayOfTodos.length);
+        localStorage.clear();
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
